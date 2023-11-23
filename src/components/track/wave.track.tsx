c3a9f1495966bb602a7058c98ab545be72d63201
DuongPC <pduong244@gmail.com>
2023-11-23 16:49:44 +0700

    Layout Audio

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -9,53 +9,71 @@ import React, {
 } from "react";
 import { WaveSurferOptions } from "wavesurfer.js";
 import "./wave.track.scss";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 function WaveTrack(props: any) {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [time, setTime] = useState<string>("0:00");
+  const [duration, setDuration] = useState<string>("0:00");
+  const hoverRef = useRef<HTMLDivElement>(null);
+
   const { audio } = props;
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const optionsMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d")!;
+    let gradient, progressGradient;
+    if (typeof window !== "undefined") {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d")!;
 
-    // Define the waveform gradient
-    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
-    gradient.addColorStop(0, "#656666"); // Top color
-    gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
-    gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff"); // White line
-    gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#ffffff"); // White line
-    gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1"); // Bottom color
-    gradient.addColorStop(1, "#B1B1B1"); // Bottom color
-    // Define the progress gradient
-    const progressGradient = ctx.createLinearGradient(
-      0,
-      0,
-      0,
-      canvas.height * 1.35
-    );
-    progressGradient.addColorStop(0, "#EE772F"); // Top color
-    progressGradient.addColorStop(
-      (canvas.height * 0.7) / canvas.height,
-      "#EB4926"
-    ); // Top color
-    progressGradient.addColorStop(
-      (canvas.height * 0.7 + 1) / canvas.height,
-      "#ffffff"
-    ); // White line
-    progressGradient.addColorStop(
-      (canvas.height * 0.7 + 2) / canvas.height,
-      "#ffffff"
-    ); // White line
-    progressGradient.addColorStop(
-      (canvas.height * 0.7 + 3) / canvas.height,
-      "#F6B094"
-    ); // Bottom color
-    progressGradient.addColorStop(1, "#F6B094"); // Bottom color
-
+      // Define the waveform gradient
+      gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+      gradient.addColorStop(0, "#656666"); // Top color
+      gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 1) / canvas.height,
+        "#ffffff"
+      ); // White line
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 2) / canvas.height,
+        "#ffffff"
+      ); // White line
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 3) / canvas.height,
+        "#B1B1B1"
+      ); // Bottom color
+      gradient.addColorStop(1, "#B1B1B1"); // Bottom color
+      // Define the progress gradient
+      progressGradient = ctx.createLinearGradient(
+        0,
+        0,
+        0,
+        canvas.height * 1.35
+      );
+      progressGradient.addColorStop(0, "#EE772F"); // Top color
+      progressGradient.addColorStop(
+        (canvas.height * 0.7) / canvas.height,
+        "#EB4926"
+      ); // Top color
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 1) / canvas.height,
+        "#ffffff"
+      ); // White line
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 2) / canvas.height,
+        "#ffffff"
+      ); // White line
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 3) / canvas.height,
+        "#F6B094"
+      ); // Bottom color
+      progressGradient.addColorStop(1, "#F6B094"); // Bottom color
+    }
     return {
       waveColor: gradient,
       progressColor: progressGradient,
-      barWidth: 2,
+      barWidth: 3,
+      height: 100,
       url: `/api?audio=${audio}`,
     };
   }, []);
@@ -70,19 +88,24 @@ function WaveTrack(props: any) {
   useEffect(() => {
     if (!wavesurfer) return;
     setIsPlaying(false);
-    const timeEl = document.querySelector("#time")!;
-    const durationEl = document.querySelector("#duration")!;
+    const hover = hoverRef.current!;
+    const waveform = containerRef.current!;
+    waveform.addEventListener(
+      "pointermove",
+      (e) => (hover.style.width = `${e.offsetX}px`)
+    );
     const subscriptions = [
       wavesurfer.on("play", () => setIsPlaying(true)),
       wavesurfer.on("pause", () => setIsPlaying(false)),
-      wavesurfer.on(
-        "decode",
-        (duration) => (durationEl.textContent = formatTime(duration))
-      ),
-      wavesurfer.on(
-        "timeupdate",
-        (currentTime) => (timeEl.textContent = formatTime(currentTime))
-      ),
+      wavesurfer.on("decode", (duration) => {
+        setDuration(formatTime(duration));
+      }),
+      wavesurfer.on("timeupdate", (currentTime) => {
+        setTime(formatTime(currentTime));
+      }),
+      wavesurfer.once("interaction", () => {
+        wavesurfer.play();
+      }),
     ];
 
     return () => {
@@ -98,20 +121,104 @@ function WaveTrack(props: any) {
   };
 
   return (
-    <>
+    <div style={{ marginTop: 20 }}>
       <div
-        ref={containerRef}
-        style={{ minHeight: "120px" }}
-        className="wave-form-container"
+        style={{
+          display: "flex",
+          gap: 15,
+          padding: 20,
+          height: 400,
+          background:
+            "linear-gradient(135deg, rgb(106,112,67) 0%,rgb(11,15,20) 100%",
+        }}
       >
-        <div id="time">0:00</div>
-        <div id="duration">0:00</div>
-        <div id="hover"></div>
+        <div
+          className="left"
+          style={{
+            width: "75%",
+            height: "calc(100% - 10px)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className="info" style={{ display: "flex" }}>
+            <div>
+              <div
+                onClick={onPlayClick}
+                style={{
+                  borderRadius: "50%",
+                  background: "#f50",
+                  width: "50px",
+                  height: "50px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                }}
+              >
+                {isPlaying ? (
+                  <PauseIcon sx={{ fontSize: 30, color: "white" }} />
+                ) : (
+                  <PlayArrowIcon sx={{ fontSize: 30, color: "white" }} />
+                )}
+              </div>
+            </div>
+            <div style={{ marginLeft: 20 }}>
+              <div
+                style={{
+                  padding: "0 5px",
+                  background: "#333",
+                  fontSize: 30,
+                  width: "fit-content",
+                  color: "white",
+                }}
+              >
+                DuongPC
+              </div>
+              <div
+                style={{
+                  padding: "0 5px",
+                  marginTop: 10,
+                  background: "#333",
+                  fontSize: 20,
+                  width: "fit-content",
+                  color: "white",
+                }}
+              >
+                DuongPC
+              </div>
+            </div>
+          </div>
+          <div ref={containerRef} className="wave-form-container">
+            <div className="time">{time}</div>
+            <div className="duration">{duration}</div>
+            <div ref={hoverRef} className="hover-wave"></div>
+            <div
+              className="overlay"
+              style={{
+                position: "absolute",
+                height: "30px",
+                width: "100%",
+                bottom: "0",
+                backdropFilter: "brightness(0.5)",
+              }}
+            ></div>
+          </div>
+        </div>
+        <div
+          className="right"
+          style={{
+            width: "25%",
+            height: "calc(100% - 10px)",
+            padding: 15,
+            display: "flex",
+            alignItems: "center",
+            background: "#CDCBCD",
+          }}
+        ></div>
       </div>
-      <button onClick={onPlayClick} style={{ marginTop: "1em" }}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
-    </>
+    </div>
   );
 }
 
