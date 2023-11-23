c5faa3fc76c204a4cc92d25b8c865dde2063f661
DuongPC <pduong244@gmail.com>
2023-11-23 14:48:02 +0700

    Interact With Wave Surfer

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,24 +1,61 @@
 "use client";
-import WaveSurfer from "wavesurfer.js";
-import { useEffect, useRef } from "react";
+import { useWavesurfer } from "@/utils/customHook";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
 
 function WaveTrack(props: any) {
-  const ref = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   const { audio } = props;
-
-  useEffect(() => {
-    const wavesurfer = WaveSurfer.create({
-      container: ref.current!,
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const optionsMemo = useMemo(() => {
+    return {
       waveColor: "rgb(200, 0, 200)",
       progressColor: "rgb(100, 0, 100)",
       url: `/api?audio=${audio}`,
-    });
-    wavesurfer.on("click", () => {
-      wavesurfer.play();
-    });
+    };
   }, []);
+  const wavesurfer = useWavesurfer(containerRef, optionsMemo);
 
-  return <div ref={ref}></div>;
+  const onPlayClick = useCallback(() => {
+    wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
+  }, [wavesurfer]);
+
+  useEffect(() => {
+    if (!wavesurfer) return;
+
+    setCurrentTime(0);
+    setIsPlaying(false);
+
+    const subscriptions = [
+      wavesurfer.on("play", () => setIsPlaying(true)),
+      wavesurfer.on("pause", () => setIsPlaying(false)),
+      wavesurfer.on("timeupdate", (currentTime: number) =>
+        setCurrentTime(currentTime)
+      ),
+    ];
+
+    return () => {
+      subscriptions.forEach((unsub) => unsub());
+    };
+  }, [wavesurfer]);
+
+  return (
+    <>
+      <div ref={containerRef} style={{ minHeight: "120px" }} />
+
+      <button onClick={onPlayClick} style={{ marginTop: "1em" }}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+
+      <p>Seconds played: {currentTime}</p>
+    </>
+  );
 }
 
 export default WaveTrack;
