45ee4a045949b3a832f54df431477f84c860fd38
DuongPC <pduong244@gmail.com>
2023-11-24 11:19:37 +0700

    Customize Session 2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,42 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
+import { sendRequest } from "@/utils/api";
+import { JWT } from "next-auth/jwt";
 
 export const authOptions: AuthOptions = {
   secret: process.env.NO_SECRET,
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async jwt({ token, user, account, profile, trigger }) {
       if (trigger === "signIn" && account?.provider === "github") {
-        token.address = "abc";
+        const res = await sendRequest<IBackendRes<JWT>>({
+          url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/social-media`,
+          method: "POST",
+          body: {
+            type: "GITHUB",
+            username: user.email,
+          },
+        });
+        if (res.data) {
+          token.access_token = res.data?.access_token;
+          token.refresh_token = res.data?.refresh_token;
+          token.user = res.data?.user;
+        }
       }
       return token;
     },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.address = token.address;
+      if (token) {
+        session.access_token = token.access_token;
+        session.refresh_token = token.refresh_token;
+        session.user = token.user;
+      }
       return session;
     },
   },
