7a0836e66bb9d8201fdcfd4dffd6f4a0cffc8d91
DuongPC <pduong244@gmail.com>
2023-11-24 11:39:59 +0700

    Login with Credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,33 @@ import GithubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
 import { sendRequest } from "@/utils/api";
 import { JWT } from "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
   secret: process.env.NO_SECRET,
   providers: [
+    CredentialsProvider({
+      name: "classic way",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await sendRequest<IBackendRes<JWT>>({
+          url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
+          method: "POST",
+          body: {
+            username: credentials?.username,
+            password: credentials?.password,
+          },
+        });
+        if (res && res.data) {
+          return res.data as any;
+        } else {
+          return null;
+        }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -14,12 +37,12 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user, account, profile, trigger }) {
-      if (trigger === "signIn" && account?.provider === "github") {
+      if (trigger === "signIn" && account?.provider !== "credentials") {
         const res = await sendRequest<IBackendRes<JWT>>({
           url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/social-media`,
           method: "POST",
           body: {
-            type: "GITHUB",
+            type: account?.provider?.toLocaleUpperCase(),
             username: user.email,
           },
         });
@@ -28,6 +51,11 @@ export const authOptions: AuthOptions = {
           token.refresh_token = res.data?.refresh_token;
           token.user = res.data?.user;
         }
+      }
+      if (trigger === "signIn" && account?.provider === "credentials") {
+        token.access_token = user.access_token;
+        token.refresh_token = user.refresh_token;
+        token.user = user.user;
       }
       return token;
     },
